6faea45c8b5a86b51777d13de79adc7b73580ac9
mistgold <33057464+mistgold@users.noreply.github.com>
2018-01-19 13:52:33 +0100

    Initial commit

---
+++ gatelessgate-Feathercoin-platform0.bat
@@ -0,0 +1,8 @@
+@echo off
+set GPU_FORCE_64BIT_PTR 1
+set GPU_MAX_HEAP_SIZE 100
+set GPU_USE_SYNC_OBJECTS 1
+set GPU_MAX_ALLOC_PERCENT 100
+set GPU_SINGLE_ALLOC_PERCENT 100
+gatelessgate.exe --gpu-platform 0 --default-config gatelessgate-Feathercoin.conf
+pause
